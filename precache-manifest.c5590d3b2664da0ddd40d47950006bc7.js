acb83e8091173e652f52be0ba57a84692a4398ba
usman998 <us.mohd1998@gmail.com>
2020-02-08 03:26:11 -0800

    Updates

---
+++ precache-manifest.c5590d3b2664da0ddd40d47950006bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b9527e6d2c32ad0e2378755c519e81",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "8ffc32268d4546eac2f7",
+    "url": "/monsters-rolodex2/static/css/main.3ecb9623.chunk.css"
+  },
+  {
+    "revision": "5c1eecf34172a552a9b2",
+    "url": "/monsters-rolodex2/static/js/2.d29a3107.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex2/static/js/2.d29a3107.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ffc32268d4546eac2f7",
+    "url": "/monsters-rolodex2/static/js/main.ba90b85a.chunk.js"
+  },
+  {
+    "revision": "95c9ab00c0f78e22b75b",
+    "url": "/monsters-rolodex2/static/js/runtime-main.e1f10adf.js"
+  }
+]);
